d917def2d421d547884c1fc709cec94c99bfe6b4
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-01-30 16:46:27 +0300

    todo class add

---
+++ ToDoList/Data/PriorityType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Data
+{
+    public class PriorityType
+    {
+        public string MyProperty { get; set; }
+    }
+}
