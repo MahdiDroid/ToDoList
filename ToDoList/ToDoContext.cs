f5ad4c89fc45fb0f795665f7b80baa4fba755a99
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-01-30 17:01:21 +0300

    connection srting added dbcontext class added

--- ToDoList/ToDoContext.cs
+++ ToDoList/ToDoContext.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using ToDoList.Data;
 
 namespace ToDoList
 {
     public class ToDoContext : DbContext
     {
-        public DbSet<> MyProperty { get; set; }
+        public DbSet<PriorityType> PriorityTypes { get; set; }
+        public DbSet<ToDo> ToDos { get; set; }
+
     }
 }
