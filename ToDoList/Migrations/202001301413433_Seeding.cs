06472d896931e2e017343dbecb1010d716b89dd5
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-01-30 17:32:43 +0300

    propryType tabele seeded

---
+++ ToDoList/Migrations/202001301413433_Seeding.cs
@@ -0,0 +1,24 @@
+﻿namespace ToDoList.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Seeding : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO PriorityTypes (Name) VALUES ('Low')");
+            Sql("INSERT INTO PriorityTypes (Name) VALUES ('Medium')");
+            Sql("INSERT INTO PriorityTypes (Name) VALUES ('High')");
+
+
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
+
+
+
