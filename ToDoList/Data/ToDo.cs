c88bf1f845c77d8c74b3b0a5320c907c3170df72
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-01-30 16:58:22 +0300

    is done propperty to to do class add

--- ToDoList/Data/ToDo.cs
+++ ToDoList/Data/ToDo.cs
@@ -10,6 +10,7 @@ namespace ToDoList.Data
         public int Id { get; set; }
         public string Title { get; set; }
         public DateTime Date { get; set; }
+        public bool IsDone { get; set; }
         public string Description { get; set; }
         public int PriorityTypeId { get; set; }
         public PriorityType PriorityType { get; set; }
