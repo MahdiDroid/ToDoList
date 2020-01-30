4ad52d74be7cd74a558119a1b2b78348b03c7666
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-01-30 17:34:48 +0300

    TodOTable Seeded

---
+++ ToDoList/Migrations/202001301433215_SeedingToDoTable.cs
@@ -0,0 +1,18 @@
+﻿namespace ToDoList.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedingToDoTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO ToDoes (Title,Date,IsDone,Description,PriorityTypeId) VALUES ('Shopping','2020-01-30',0,'Monthly shoping',10)");
+
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
