dffbb00697ee1cbc13fc03ed67507c2ddc64ef1b
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-01 11:01:22 +0300

    MVC own crud added

---
+++ ToDoList/Controllers/ToDoesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ToDoList;
+using ToDoList.Data;
+
+namespace ToDoList.Controllers
+{
+    public class ToDoesController : Controller
+    {
+        private ToDoContext db = new ToDoContext();
+
+        // GET: ToDoes
+        public ActionResult Index()
+        {
+            var toDos = db.ToDos.Include(t => t.PriorityType);
+            return View(toDos.ToList());
+        }
+
+        // GET: ToDoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDo toDo = db.ToDos.Find(id);
+            if (toDo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toDo);
+        }
+
+        // GET: ToDoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.PriorityTypeId = new SelectList(db.PriorityTypes, "Id", "Name");
+            return View();
+        }
+
+        // POST: ToDoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Date,IsDone,Description,PriorityTypeId")] ToDo toDo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ToDos.Add(toDo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PriorityTypeId = new SelectList(db.PriorityTypes, "Id", "Name", toDo.PriorityTypeId);
+            return View(toDo);
+        }
+
+        // GET: ToDoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDo toDo = db.ToDos.Find(id);
+            if (toDo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PriorityTypeId = new SelectList(db.PriorityTypes, "Id", "Name", toDo.PriorityTypeId);
+            return View(toDo);
+        }
+
+        // POST: ToDoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Date,IsDone,Description,PriorityTypeId")] ToDo toDo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(toDo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PriorityTypeId = new SelectList(db.PriorityTypes, "Id", "Name", toDo.PriorityTypeId);
+            return View(toDo);
+        }
+
+        // GET: ToDoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDo toDo = db.ToDos.Find(id);
+            if (toDo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toDo);
+        }
+
+        // POST: ToDoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ToDo toDo = db.ToDos.Find(id);
+            db.ToDos.Remove(toDo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
