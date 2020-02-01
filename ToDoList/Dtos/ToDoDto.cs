3d54e40a86bf435d7095ee19191494931594b7ec
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-01 15:35:44 +0300

    MapperProfile class added initilized

---
+++ ToDoList/Dtos/ToDoDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Dtos
+{
+    public class ToDoDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsDone { get; set; }
+        public string Description { get; set; }
+        public int PriorityTypeId { get; set; }
+    }
+}
